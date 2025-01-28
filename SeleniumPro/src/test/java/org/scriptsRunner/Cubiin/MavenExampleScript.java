2dd96280862eb15aa76572baafb6a618f74c4c6c
Divya Gururaj <Divya Gururaj@DESKTOP-26C0NPL>
2025-01-28 16:19:01 +0530

    TRY

---
+++ SeleniumPro/src/test/java/org/scriptsRunner/Cubiin/MavenExampleScript.java
@@ -0,0 +1,19 @@
+package org.scriptsRunner.Cubiin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MavenExampleScript 
+{
+	@Test
+	public void launch()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.close();
+	}
+}
