d5dc3c4da312ef079f8c4be68316066bcf74f40e
Dell <Dell@DESKTOP-03F20BQ.mshome.net>
2025-01-28 17:13:28 +0530

    Added a new class

---
+++ SeleniumPro/src/test/java/org/scriptsRunner/Cubiin/NewClass.java
@@ -0,0 +1,13 @@
+package org.scriptsRunner.Cubiin;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("TestCase ===== PASS");
+	}
+	
+}
